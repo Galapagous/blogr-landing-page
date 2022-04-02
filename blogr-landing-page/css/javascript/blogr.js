b0b6fb46375eeeb7b788ae09ecd389cd8dbe84da
Muhammed MUsa <72840992+Galapagous@users.noreply.github.com>
2022-04-02 16:55:50 +0100

    Delete blogr.js

--- blogr-landing-page/css/javascript/blogr.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(".hamburger-menu")
-const siteNav = document.querySelector(".site-nav")
-
-// some petty functions
-hamburger.addEventListener("click", (e) => {
-  hamburger.classList.toggle("active")
-  siteNav.classList.toggle("active")
-})
